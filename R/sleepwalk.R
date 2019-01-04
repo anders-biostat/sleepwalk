ceb287000d45653c992e410b6919f26d8dd54305
Unknown <svetlana.ovchinnikova@helsinki.fi>
2019-01-04 13:15:32 +0100

    maxdists can be estimated automatically

--- R/sleepwalk.R
+++ R/sleepwalk.R
@@ -7,7 +7,7 @@
 }
 
 #' @export
-sleepwalk <- function( embeddings, featureMatrices, maxdists, pointSize = 1.5, same = c( "objects", "features" ) ) {
+sleepwalk <- function( embeddings, featureMatrices, maxdists = NULL, pointSize = 1.5, same = c( "objects", "features" ) ) {
   same = match.arg( same )
   
   #if there is only one embedding
@@ -19,11 +19,21 @@ sleepwalk <- function( embeddings, featureMatrices, maxdists, pointSize = 1.5, s
   stopifnot( is.list(embeddings) )
   stopifnot( is.list(featureMatrices) )
   
-  stopifnot( is.numeric(maxdists) )
   stopifnot( is.numeric(pointSize) && length(pointSize) == 1 )
     
   stopifnot( length(embeddings) <= 9 )
   
+  #estimate maxdists from the data
+  if(is.null(maxdists)) {
+    maxdists <- sapply(1:length(featureMatrices), function(i) {
+      message(paste0("Estimating 'maxdist' for feature matrix "), i)
+      pairs <- cbind(sample(nrow(featureMatrices[[i]]), 1500, TRUE), 
+                     sample(nrow(featureMatrices[[i]]), 1500, TRUE))
+      quantile(sqrt(rowSums((featureMatrices[[i]][pairs[, 1], ] - featureMatrices[[i]][pairs[, 2], ])^2)), 0.5) 
+    })
+  }
+  stopifnot( is.numeric(maxdists) )
+    
   if( same == "objects" ) {
     stopifnot( length(embeddings) == length(featureMatrices) | length(featureMatrices) == 1 )
     stopifnot( length(maxdists) == length(featureMatrices) )
