eecc305e4d4e3a5ba87b862b7b6946e116bb35c6
Sveta <tilakloivenn@gmail.com>
2021-06-25 14:44:38 +0200

    limitStorage replaced with setLimits

--- R/sleepwalk.R
+++ R/sleepwalk.R
@@ -254,7 +254,7 @@ sleepwalk <- function( embeddings, featureMatrices = NULL, maxdists = NULL, poin
     jrc::openPage( useViewer = FALSE, 
                    rootDirectory = system.file( package="sleepwalk" ),
                    startPage = paste0(system.file( package="sleepwalk" ),"/sleepwalk_", mode, ".html"), ... )
-    jrc::limitStorage(n = 0)
+    jrc::setLimits(storedMsg = 0)
     jrc::allowFunctions(c("message", ".slw$on_selection"))
     jrc::allowVariables(c("embs", "dists", "pointSize", "maxdists", "titles", "finished"))
     
